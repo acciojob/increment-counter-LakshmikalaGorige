110b1a79a310a5991b2f11e23037abda7c4cec51
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-25 19:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let a=document.getElementById("incrementBtn");
+
+let counter=0;
+a.addEventListener("click",()=>{
+	counter=counter+1;
+	document.getElementById("counter").innerText=counter;
+});
